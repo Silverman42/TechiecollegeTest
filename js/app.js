4d8c8069d491f63ec75a50eb0d369f07b390f916
Sylvester Nkeze <snkeze9@gmail.com>
2024-08-10 11:11:47 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+//Data
+// Numbers
+// String
+// Arrays
+// Objects
+
+//Variables
+//Constant
+
+let names = 1 // Variable declaration
+
+names = 2
+
+names = 3
+
+const school = "St Johns"
+
+let age_1 = 235 //float
+
+let age_2 = 2325 //float
+
+let hospitals = "235" //string
+
+let result = age_1 + hospitals  //concatination
+
+let multiplier = [ "Naira" , 2 , 'James' , "John" , 5 , 6 , 7 , 8 , 9 , 10 , 11, 12] //index
+
+result = multiplier[0] + multiplier[5]
+
+console.log(result)
+
+// for(let i = 0; i < multiplier.length; i++){ //looping
+//     console.log( 2 * multiplier[i]);
+// }
